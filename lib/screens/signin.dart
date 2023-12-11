58fbc43b24feaffb8fc6764333db2c8dadcb45c5
HAFIDI Aymane <ahafidi@bordeaux-inp.fr>
2023-12-11 12:59:43 +0100

    Android branch

--- lib/screens/signin.dart
+++ lib/screens/signin.dart
@@ -3,7 +3,6 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:quizart/screens/QuizChoose.dart';
 import 'package:quizart/services/auth.dart';
 import 'package:quizart/services/toast.dart';
-import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 
 
@@ -15,7 +14,6 @@ class UserSignIn extends StatefulWidget {
 }
 
 class _UserSignInState extends State<UserSignIn> {
-  
   final AuthentService _auth = AuthentService();
 
   final TextEditingController _emailController = TextEditingController();
@@ -31,73 +29,99 @@ class _UserSignInState extends State<UserSignIn> {
 
   final _formKey = GlobalKey<FormState>();
   @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('LogIn'),
-      ),
-      body: Padding(
-        padding: const EdgeInsets.all(16.0),
-        child: Form(
-          key: _formKey,
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.stretch,
-            children: <Widget>[
+Widget build(BuildContext context) {
+  var backgroundImage = 'assets/images/background_LOGIN_page.png'; // Replace with your actual asset path
 
-              _buildTextField('Email', _emailController,
-                  keyboardType: TextInputType.emailAddress,
-                  validator: _validateEmail),
-              const SizedBox(height: 20),
-              TextFormField(
-                obscureText: true,
-                controller: _passwordController,
-                decoration: const InputDecoration(
-                  labelText: 'Password',
-                  border: OutlineInputBorder(),
-                ),
+  return Scaffold(
+    backgroundColor: Colors.white,
+    body: SingleChildScrollView(
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: <Widget>[
+          // Background image with text on top
+          Container(
+            height: MediaQuery.of(context).size.height * 0.60, // Adjust the height as needed
+            decoration: BoxDecoration(
+              image: DecorationImage(
+                image: AssetImage(backgroundImage),
+                fit: BoxFit.cover,
               ),
-              const SizedBox(height: 20),
-
-              ElevatedButton(
-                onPressed: ()async {
-
-                  _submitFormSignIn();
-                },
-                child: const Text('Connect'),
+            ),
+            alignment: Alignment.center,
+            child: const Text(
+              'Welcome To PollArt',
+              style: TextStyle(
+                fontSize: 30,
+                fontWeight: FontWeight.bold,
+                color: Colors.white,
               ),
-              const SizedBox(height: 16.0), // Espacement entre les boutons
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  ElevatedButton.icon(
-                    onPressed: () {
+              textAlign: TextAlign.center,
+            ),
+          ),
+          // Form starts here
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 16),
+            child: Form(
+              key: _formKey,
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.start, // Align form at the top of the remaining space
+                crossAxisAlignment: CrossAxisAlignment.stretch,
+                children: <Widget>[
+                  SizedBox(height: 0), // Space between image and form
+                  _buildTextField('Email', _emailController,
+                      keyboardType: TextInputType.emailAddress,
+                      validator: _validateEmail),
+                  SizedBox(height: 20),
+                  TextFormField(
+                    obscureText: true,
+                    controller: _passwordController,
+                    decoration: const InputDecoration(
+                      labelText: 'Password',
+                      border: OutlineInputBorder(),
+                    ),
+                  ),
+                  SizedBox(height: 20),
+                  ElevatedButton(
+                    onPressed: () async {
+                      _submitFormSignIn();
                     },
-                    icon: const FaIcon(FontAwesomeIcons.google),
-                    label: const Text('Connect with Google',style: TextStyle(color:Colors.white,
-                    fontWeight: FontWeight.bold)),
+                    child: const Text('Connect'),
+                    style: ElevatedButton.styleFrom(
+                      primary: const Color.fromRGBO(109, 114, 224, 1), // background
+                      onPrimary: Colors.white,
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(32.0),
+                      ),
+                    ),
+                  ),
+                  SizedBox(height: 16.0),
+                  TextButton(
+                    onPressed: () {
+                      Navigator.pushNamed(context, '/user_info_form');
+                    },
+                    style: TextButton.styleFrom(
+                      primary: Colors.blue, // Text color
+                      backgroundColor: const Color.fromARGB(0, 0, 0, 0), // Transparent background
+                      tapTargetSize: MaterialTapTargetSize.shrinkWrap, // Minimizes the tap area to the size of the child
+                      padding: EdgeInsets.zero, // No padding
+                    ),
+                    child: Text(
+                      'Not registered? Register here',
+                      style: TextStyle(
+                        decoration: TextDecoration.underline, // Underline to indicate it's a link
+                      ),
+                    ),
                   ),
                 ],
               ),
-              GestureDetector(
-                onTap: () {
-                  // Navigation vers la page d'inscription
-                  Navigator.pushNamed(context, '/user_info_form');
-                },
-                child: const Text(
-                  'Pas encore inscrit ? Créer un compte',
-                  style: TextStyle(
-                    color: Colors.blue,
-                    decoration: TextDecoration.underline,
-                  ),
-                  textAlign: TextAlign.center,
-                ),
-              ),
-            ],
+            ),
           ),
-        ),
+        ],
       ),
-    );
-  }
+    ),
+  );
+}
+
 
   Widget _buildTextField(String label, TextEditingController controller,
       {TextInputType? keyboardType, String? Function(String?)? validator}) {
@@ -112,35 +136,41 @@ class _UserSignInState extends State<UserSignIn> {
     );
   }
 
-String? _validateEmail(String? value) =>
-    value?.isEmpty ?? true
-        ? 'Email requis'
-        : !RegExp(r"^[ a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$").hasMatch(value!)
-            ? 'Email invalide'
-            : null;
-
-
-void _submitFormSignIn() async {
-  if (_formKey.currentState?.validate() ?? false) {
-    final email = _emailController.text;
-    final password = _passwordController.text;
-
-    final user = await _auth.SignIn(email, password);
-
-    user != null
-        ? _handleSuccessfulSignIn(user)
-        : showToast(message: "not registered!");
+  String? _validateEmail(String? value) {
+    if (value == null || value.isEmpty) {
+      return 'Email requis';
+    }
+    if (!RegExp(r"^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$")
+        .hasMatch(value)) {
+      return 'Email invalide';
+    }
+    return null;
   }
-}
 
-void _handleSuccessfulSignIn(User user) {
-  showToast(message: "user connected");
-  Navigator.pushReplacement(
-    context,
-    MaterialPageRoute(
-      builder: (context) => QuizChoose(uid: user.uid),
-    ),
-  );
-}
 
+  void _submitFormSignIn() async{
+    if (_formKey.currentState?.validate() ?? false) {
+      // Form is valid, handle form submission here
+      setState((){
+      });
+      String email = _emailController.text;
+      String password = _passwordController.text;
+
+      User? user = await _auth.SignIn(email, password);
+
+      setState((){
+      });
+      if(user != null){
+        showToast(message:"user connected");
+        Navigator.pushReplacement(
+          context,
+          MaterialPageRoute(
+            builder: (context) => QuizChoose(uid: user.uid),
+          ),
+        );
+      }else{
+        showToast(message:"not registered!");
+      }
+    }
+  }
 }
